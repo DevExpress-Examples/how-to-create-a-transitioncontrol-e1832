77fb33bfb6893e9769eda30a3adbdb2e5f8f5064
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Create a TransitionControl
+
+
+<p>This example shows how to use a TransitionControl.</p>
+
+<br/>
+
+
---
+++ VB/AgCore_Using_AgTransitionControl/MainPage.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace AgCore_Using_AgTransitionControl
+	Partial Public Class MainPage
+		Inherits UserControl
+		Private index As Integer
+		Public Sub New()
+			InitializeComponent()
+			index = 0
+			trControl.Content = GetElement("r" & index.ToString())
+		End Sub
+
+		Private Sub trControl_MouseLeftButtonUp(ByVal sender As Object, _
+				ByVal e As System.Windows.Input.MouseButtonEventArgs)
+			If index = 2 Then
+				index = 0
+			Else
+				index += 1
+			End If
+			trControl.Content = GetElement("r" & index.ToString())
+		End Sub
+		Private Function GetElement(ByVal id As String) As FrameworkElement
+			Return CType((CType(LayoutRoot.Resources(id), _
+				DataTemplate)).LoadContent(), FrameworkElement)
+		End Function
+	End Class
+End Namespace
